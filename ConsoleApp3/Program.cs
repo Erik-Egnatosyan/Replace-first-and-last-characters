9c9e93be863c14a90a3008861d213ca42aac68f5
Sansacia <egnatosyan.sans@mail.ru>
2022-06-21 00:41:47 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(First_last("Codenyx"));
+            Console.WriteLine(First_last("Java"));
+        }
+
+        private static string First_last(string ustr)
+        {
+            return ustr.Length > 1
+                ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
+        }
+    }
+}
